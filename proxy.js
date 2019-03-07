d385a0f4d14636dde5fd9c4bb03ba60df7252b2a
shuklagopi87 <48189265+shuklagopi87@users.noreply.github.com>
2019-03-07 10:16:52 -0500

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ var exec= require('child_process').exec,
     fs=require('fs')
 
    
-
+// We download all the ressources from the Google Sheet
 https.get('https://spreadsheets.google.com/feeds/list/1wvXbbPwfI8KZwu_OhbH5-0DS3f9tcOcGxg5ITwgUS6c/1/public/values?alt=json'
 , function(resp){
   var  data = '';
@@ -15,7 +15,10 @@ https.get('https://spreadsheets.google.com/feeds/list/1wvXbbPwfI8KZwu_OhbH5-0DS3
   });
 
   resp.on('end', function() {
-    check(JSON.parse(data).feed.entry.map(function(d){return d.gsx$url.$t}),function(result){
+      // All the data is downloaded and is checked
+      var thedata=JSON.parse(data).feed.entry.map(function(d){return d.gsx$url.$t})
+    checkall(thedata,[],function(result){
+        // The result is saved in a file
         fs.writeFileSync("result.json",JSON.stringify(result))
     })
   });
@@ -25,21 +28,10 @@ https.get('https://spreadsheets.google.com/feeds/list/1wvXbbPwfI8KZwu_OhbH5-0DS3
 });
 
 
-    
 
-
-function check(data,next){
-    console.log(data[0])
-    var res=[]
-    checkall(data.slice(0,500),[],function(result){
-      next(result)
-    })
-    
-}
 function checkall(alldata,result,next){
-  console.log(alldata.length)
-  //var res=[]
-  var r=alldata.pop()
+    // alldata is an array of URLs. We take one URL
+    var r=alldata.pop()
   if (r) {
     checkoneurl(r,function(d){
       result.push(d)
@@ -52,13 +44,13 @@ function checkall(alldata,result,next){
 }
 
 function checkoneurl(url,next){
-  console.log(url)
-var e='curl -x http://lahore.wonderproxy.com:12000 --proxy-user "marcomerens:785NEWautomobile" -L "'+url+'" -o /dev/null -w "%{http_code}"'
+// We check the URL by executing curl using the proxy located in Lahore in Pakistan    
+var e='curl -x http://lahore.wonderproxy.com:12000 --proxy-user "USERNAME:PASSWORD" -L "'+url+'" -o /dev/null -w "%{http_code}"'
 
-//var e='curl -s -o /dev/null -w "%{http_code}" -x 124.109.53.100:8888 "'+url+'"'
 exec(e, (err, stdout, stderr) => {
     console.log(stderr)
     console.log(err)
+    // We store the result of the call and any errors which may have occurred
     next({url:url,result:{stdout:stdout,stderr:stderr,err:(err?JSON.stringify(err):"")}})
 });
 }
